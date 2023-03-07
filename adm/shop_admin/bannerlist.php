2a24dc47323687ccbf4edb91ec5c755938d1b4c1
RUNZ <75666458+elfe0117@users.noreply.github.com>
2023-03-07 17:36:40 +0900

    쇼핑몰 브랜드 관리

--- adm/shop_admin/bannerlist.php
+++ adm/shop_admin/bannerlist.php
@@ -185,8 +185,9 @@ $colspan = 9;
 
     <?php
     }
+    
     if ($i == 0) {
-    echo '<tr><td colspan="'.$colspan.'" class="empty_table">자료가 없습니다.</td></tr>';
+        echo '<tr><td colspan="'.$colspan.'" class="empty_table">자료가 없습니다.</td></tr>';
     }
     ?>
     </tbody>
